c6f46df507759bc6168ab144f651f5a50bbb45e5
ahmetyanik <ahmetyank4242@gmail.com>
2021-01-11 22:23:03 +0100

    version

--- KitapSitesi/index.js
+++ KitapSitesi/index.js
@@ -1,15 +1,13 @@
 const multer = require("multer");
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, __dirname+'/dosyalar/resimler')
+    cb(null, __dirname + '/dosyalar/resimler')
   },
   filename: function (req, file, cb) {
-    cb(null, file.fieldname + '-' + Date.now()+'.jpg')
+    cb(null, file.fieldname + '-' + Date.now() + '.jpg')
   }
 });
-
 var upload = multer({ storage: storage });
-
 const mysql = require("mysql");
 const bodyParser = require("body-parser");
 const express = require("express");
@@ -18,8 +16,6 @@ app.use(bodyParser.urlencoded( {extended: true} ));
 app.set("view engine" , "ejs");
 app.use(express.static(__dirname + "/dosyalar"));
 app.use(bodyParser.json());
-
-
 var connection = mysql.createConnection({
   multipleStatements : true,
   host     : 'localhost',
@@ -27,18 +23,12 @@ var connection = mysql.createConnection({
   password : '12344321',
   database : 'bilgiler'
 });
-
-
 connection.connect(function(err){
   if(err) throw err;
   console.log("MYSQL'e bağlandı..");
 });
-
-
-
 var kategoriler = [] ;
 // ya bir kere alsak, bir daha bu fonksyion çağrıldığında, direk daha önceki aldığımızı kullansak ?
-
 function kategorileriAl(callback){
   if(kategoriler.length > 0 ){
     console.log("var olan dönderildi.");
@@ -46,58 +36,38 @@ function kategorileriAl(callback){
   }else{
     connection.query("SELECT * from kategoriler", function(err, results, fields){
       kategoriler = results;
-      console.log("veri tabanından alındı.");
+      console.log("veri tabaından alındı.");
       return callback(kategoriler);
     });
   }
-};
-
-// app.get("/", function(){
-//
-//   connection.query("SELECT * FROM kitaplar", function(err, results, fields){
-//
-//     if(err) throw err;
-//
-//
-//     var veriTabaniKitaplar=results;
-//     res.render("anasayfa",{kitaplar:veriTabaniKitaplar});
-//
-//   });
-//
-//
-// });
-
-
-
+}
 app.get("/" , function(req , res){
-    connection.query("SELECT * from kitaplar  ;  SELECT * from kategoriler" , function(err, results, fields){
+    connection.query("SELECT * from kitaplar WHERE kategori = 'Roman' LIMIT 6  ; SELECT * from kategoriler ;  SELECT * from kitaplar WHERE kategori = 'Bilim' LIMIT 6 ;  SELECT * from kitaplar ORDER BY satis DESC LIMIT 5" , function(err, results, fields){
       if(err) throw err;
       // birinci sonuçta -> kitaplar        > results[0]
       // ikinci sonuçta  -> kategoriler     > results[1]
-      var veriTabaniKitaplar    = results[0];
-      var veriTabaniKategoriler = results[1];
-      console.log(veriTabaniKitaplar);
+      var veriTabani6RomanKitabi  = results[0];
+      var veriTabaniKategoriler   = results[1];
+      var veriTabani6BilimKitabi  = results[2];
+      var veriTabaniCokSatanlar   = results[3];
       res.render("anasayfa" , {
-                                  kitaplar :  veriTabaniKitaplar  ,
-                                  kategoriler : veriTabaniKategoriler}
+                                  bilimler : veriTabani6BilimKitabi,
+                                  romanlar :  veriTabani6RomanKitabi,
+                                  kategoriler : veriTabaniKategoriler,
+                                  coksatanlar : veriTabaniCokSatanlar
+                                }
                 );
     });
 });
-
-
-
-
 // kitapsitesi.com/kitap/TehlikeliOyunlar/78
 app.get("/kitap/:isim/:id", function(req, res){
     var idDegeri = req.params.id; // -> 78
     var sql = "SELECT * from kitaplar WHERE id = " + idDegeri ;
-
-
     kategorileriAl(function(kategoriler){
-
       connection.query(sql, function(err, results, fields){
           if(err) throw err;
           console.log(results);
+          var kitapId         = results[0].id;
           var kitapYazar      = results[0].yazar;
           var kitapAciklama   = results[0].aciklama;
           var kitapResim      = results[0].resimlinki;
@@ -105,16 +75,19 @@ app.get("/kitap/:isim/:id", function(req, res){
           var kitapIsmi       = results[0].kitapismi;
           var kitapFiyati     = results[0].fiyat;
           var kitapKategori   = results[0].kategori;
-          res.render("kitap" , { yazar : kitapYazar,
-                                 aciklama: kitapAciklama,
-                                 resim : kitapResim,
-                                 yayinevi : kitapYayinEvi,
-                                 isim : kitapIsmi ,
-                                 fiyat : kitapFiyati,
-                                 kategori : kitapKategori,
-                                 kitaplar : [],
-                                 kategoriler : kategoriler
-                               });
+          var sql2 = "SELECT * FROM bilgiler.kitaplar WHERE kategori = '"+kitapKategori+"' AND id != "+kitapId+" ORDER BY satis DESC LIMIT 6"
+          connection.query(sql2, function(err, results, fields){
+            res.render("kitap" , { yazar : kitapYazar,
+                                   aciklama: kitapAciklama,
+                                   resim : kitapResim,
+                                   yayinevi : kitapYayinEvi,
+                                   isim : kitapIsmi ,
+                                   fiyat : kitapFiyati,
+                                   kategori : kitapKategori,
+                                   kitaplar : results,
+                                   kategoriler : kategoriler
+                                 });
+          });
       });
     });
 });
@@ -130,23 +103,15 @@ ON bilgiler.kategoriler.kategori_link = 'bilim'
 WHERE bilgiler.kategoriler.kategori_ismi = bilgiler.kitaplar.kategori
 */
 app.get("/kategori/:kategorilink", function(req, res){
-
     kategorileriAl(function(gelenKategoriler){
       // kategoriler geldi...
         var kategoriLink = req.params.kategorilink; // edebiyat
-
         var sql = "SELECT bilgiler.kitaplar.* FROM bilgiler.kitaplar LEFT JOIN bilgiler.kategoriler ON bilgiler.kategoriler.kategori_link = '"+ kategoriLink +"' WHERE bilgiler.kategoriler.kategori_ismi = bilgiler.kitaplar.kategori"
-
-
         connection.query(sql , function(err , results, fields){
             res.render("kategori", {kitaplar : results , kategoriler : gelenKategoriler} );
         });
     });
 });
-
-
-
-
 app.get("/arama" , function(req, res){
     // veritabanına bağlanacağız, orada bu kelimeye ait
     // kitap varsa, onları kullanıcıya göstereceğiz.
@@ -160,19 +125,13 @@ app.get("/arama" , function(req, res){
       res.render("arama" , { kitaplar : bulunanKitaplar , kategoriler : bulunanKategoriler });
     });
 });
-
-
 app.get("/kitapekle", function(req, res){
     res.sendFile(__dirname + "/views/kitapekle.html");
 });
-
-
-
-app.post("/veritabanina-ekle", upload.single('dosya'), function(req, res){
-
-    var resimlinki="";
+app.post("/veritabanina-ekle"   ,  upload.single('dosya')  ,  function(req, res){
+    var resimlinki = "";
     if(req.file){
-      resimlinki="/resimler/"+req.file.filename;
+      resimlinki = "/resimler/"+req.file.filename;
     }
     var kitapismi = req.body.kitapismi;
     var yazar     = req.body.yazar;
@@ -186,7 +145,7 @@ app.post("/veritabanina-ekle", upload.single('dosya'), function(req, res){
     //kitapismi, fiyat, resimlinki, yayinevi, aciklama, yazar, kategori
     var sql = "INSERT INTO bilgiler.kitaplar (kitapismi, fiyat, resimlinki, yayinevi, aciklama, yazar, kategori) VALUES('"+kitapismi+"','"+ fiyat+"', '"+resimlinki+"' ,'"+ yayinevi + "','" + aciklama +"','"+ yazar +"','"+ kategori+"')";
     connection.query(sql, function(err, results, fields){
-      res.redirect("/");
+      res.redirect("/kitapekle");
     });
 });
 let port = process.env.PORT;
